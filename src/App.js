fe6bf0447af1a20b0b3a0e80cea1d3fc5c162879
Ciphergyst <Jeremy McGrath>
2019-09-22 15:50:29 -0700

    Functional and styled

--- src/App.js
+++ src/App.js
@@ -1,26 +1,202 @@
 import React from "react";
 import "./App.css";
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
 
-// Logo has already been provided for you. Do the same for the remaining components
+import Display from "./components/DisplayComponents/Display"
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials"
+import Numbers from  "./components/ButtonComponents/NumberButtons/Numbers"
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators"
+
+
+import {numbers, specials, operValues} from "./data"
+
 import Logo from "./components/DisplayComponents/Logo";
 
-function App() {
-  // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
-  // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
-  // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
-  // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
-  // Don't forget to pass the functions (and any additional data needed) to the components as props
 
-  return (
-    <div className="container">
-      <Logo />
-      <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+
+class App extends React.Component {
+  
+  constructor() {
+    super()
+    this.state = {
+      display: '',
+      isDone: false,
+      
+    }
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('keydown', this.handleKeyDown)
+  }
+  componentWillUnmount = () => {
+    window.removeEventListener('keydown', this.handleKeyDown)
+  }
+
+
+
+  handleKeyDown = (obj) => {
+    const keyObject = {
+      value: obj.key,
+      className: ''
+    }
+    if(keyObject.value==="Enter") {
+      keyObject.value= '='
+    }
+    if(keyObject.value==="x" || keyObject.value==="X" ) {
+      keyObject.value= '*'
+    }
+    if(keyObject.value==="Backspace") {
+      obj.preventDefault()
+      keyObject.value= 'CE'
+    }
+    if(numbers.includes(keyObject.value)) {
+      keyObject.className = "number-button"
+    } 
+    else if(operValues.includes(keyObject.value)) {
+      keyObject.className = "operator-button"
+    }
+    else if(specials.includes(keyObject.value)) {
+      keyObject.className = "special-button"
+    }
+    !(keyObject.className==='') && this.handleChange(undefined,keyObject)
+  }
+
+  
+  handleChange = (e,obj)=> {
+    let tempObj = ''
+    e ? tempObj = e.target : tempObj = obj;
+    const {value, className} = tempObj
+    
+    if(className.includes("number-button")) {
+      if(this.state.isDone) {
+        this.setState({
+          display: value,
+          isDone: false
+        })
+      }
+      else if(this.state.display==='0'){
+        this.setState({display: value})
+      }
+      else{
+        this.setState({display: this.state.display+value})
+      }
+    }
+
+    if (className.includes("operator-button")) {
+      this.setState({
+        isDone: false
+      })
+      this.handleOperators(tempObj)
+    }
+
+    if (className==="special-button") {
+      this.setState({
+        isDone: false
+      })
+      this.handleSpecials(tempObj)
+    }
+    
+  }
+
+  handleOperators = (button)=> {
+    const {value} = button
+    value==="=" 
+      ? this.handleEqual() 
+      : this.setState({
+        display: this.state.display+value
+      }
+    )
+  }
+
+  handleSpecials= (button)=> {
+    const {value} = button
+
+    //clear display
+    if(value==='C') {
+      this.setState({display: ''})
+    }
+    if(value==='+/-') {
+      this.setState({display: this.state.display*(-1)})
+      isNaN(this.state.display) && 
+        this.setState({
+          display: 'error',
+          isDone: true
+        })
+    }
+    if(value==='%') {
+      this.setState({display: this.state.display+value})
+    }
+    if(value==='CE') {
+    
+      
+      if(this.state.isDone) {
+        this.setState({
+          display: '',
+          isDone: true
+        })
+      }
+
+      
+      else {
+        this.setState({
+        display: this.state.display.slice(0, -1)
+        })
+      }
+    }
+    
+  }
+
+  handleEqual = () => {
+    try {
+      
+      const result = eval(this.state.display) 
+
+      
+      if(this.state.display!==`${result}`){
+        this.setState({
+          display: `${result}`,
+          isDone: true,
+          
+        })
+      }
+
+      
+      else {
+        this.setState({
+          isDone:true
+        })
+      }
+    } 
+    catch (e) {
+
+      this.setState({
+          display: "error",
+          isDone: true
+      })
+  }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <section className="calculator-section">
+          <Logo />
+          <div className="display-container"> 
+              <Display display={this.state.display} />
+          </div>
+          <div className="App">
+            <div className="joined-container">
+              <Specials onClick={this.handleChange}/>
+              <Numbers onClick={this.handleChange}/>
+            </div>
+            <div className="operators-container">
+              <Operators onClick={this.handleChange}/>
+            </div>
+          </div>
+        </section>
       </div>
-    </div>
-  );
+        
+    )
+  }
 }
 
 export default App;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import NumberButton from "./NumberButton"
+import {numbers} from "../../../data"
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -7,13 +9,15 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
-  // STEP 2 - add the imported data to state
+const Numbers = (props) => {
+  const numArray = numbers;
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className="numbers-container">
+      {numArray.map( ele => {
+        return <NumberButton number = {ele} onClick={props.onClick}/>
+      })}
     </div>
   );
 };
+
+export default Numbers
